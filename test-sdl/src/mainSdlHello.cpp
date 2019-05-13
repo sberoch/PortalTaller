a0c506cac2ee99b5bc87146c452515b5d08c3985
sberoch <sberoch@gmail.com>
2019-05-13 11:45:26 -0300

    mas tests sdl

--- test-sdl/src/mainSdlHello.cpp
+++ test-sdl/src/mainSdlHello.cpp
@@ -13,23 +13,50 @@ int main(int argc, char** argv){
         SdlTexture im("emisor_receptor.png", window);
         //Emisor
         Area srcArea(0, 0, 196, 191);
-        Area destArea(100, 100, 196, 191);
-        im.render(srcArea, destArea);
+        int x = 100;
+        int y = 100;
 
         //Receptor
         Area srcArea2(196, 0, 196, 191);
-        Area destArea2(450, 100, 196, 191);
-        im.render(srcArea2, destArea2);
-
-        window.render();
+        int x2 = 400;
+        int y2 = 100;
 
         SDL_Event e;
         while (!quit) {
-            while(SDL_PollEvent(&e) != 0) {
-                if(e.type == SDL_QUIT) {
+            Area destArea(x, y, 196, 191);
+            Area destArea2(x2,y2, 196, 191);
+            window.fill();
+            im.render(srcArea, destArea);
+            im.render(srcArea2, destArea2);
+            SDL_WaitEvent(&e);
+            switch (e.type) {
+                case SDL_KEYDOWN: {
+                    SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) e;
+                    switch (keyEvent.keysym.sym) {
+                        case SDLK_LEFT:
+                                x -= 30;
+                                break;
+                            case SDLK_RIGHT:
+                                x += 30;
+                                break;
+                            case SDLK_UP:
+                                y -= 30;
+                                break;
+                            case SDLK_DOWN:
+                                y += 30;
+                                break;
+                            }
+                    }
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    SDL_GetMouseState(&x2, &y2);
+                    break;
+                case SDL_QUIT:
                     quit = true;
-                }
+                    break;
+
             }
+            window.render();
         }
     } catch (std::exception& e) {
         std::cout << e.what() << std::endl;
