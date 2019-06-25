fe05eddd3a235f25538ce353479f11c2088622db
sberoch <sberoch@gmail.com>
2019-06-25 02:55:01 -0300

    fix escena juego pidiendo id antes

--- src/Cliente/EscenaJuego.h
+++ src/Cliente/EscenaJuego.h
@@ -67,7 +67,6 @@ public:
 	virtual void manejar(EventoCreacionPersonaje& evento) override;
 private:
 	void crearTerreno();
-	void recibirMiIdentificador();
 	void actualizarFondo();
 	void sleep(int tiempo);
 };
